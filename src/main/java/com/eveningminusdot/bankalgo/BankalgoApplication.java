adc0a06b0a35e2326021c3400c7ab8dc6d42498a
jhp2060 <jhp2060@gmail.com>
2020-12-09 17:30:54 +0900

    feat(): first commit

---
+++ src/main/java/com/eveningminusdot/bankalgo/BankalgoApplication.java
@@ -0,0 +1,14 @@
+package com.eveningminusdot.bankalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankalgoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BankalgoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eveningminusdot/bankalgo/BankalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eveningminusdot.bankalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankalgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
