2f74aa00b2270c48361fd65a3490ec0393813127
jhp2060 <jhp2060@gmail.com>
2020-12-12 23:41:42 +0900

    feat(setting): web application setting

--- src/main/java/com/eveningminusdot/bankalgo/BankalgoApplication.java
+++ src/main/java/com/eveningminusdot/bankalgo/BankalgoApplication.java
@@ -1,14 +1,17 @@
 package com.eveningminusdot.bankalgo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class BankalgoApplication {
-
     public static void main(String[] args) {
-
-        SpringApplication.run(BankalgoApplication.class, args);
+        // SpringApplication.run(BankalgoApplication.class, args);
+        SpringApplication application = new SpringApplication(BankalgoApplication.class);
+        application.setWebApplicationType(WebApplicationType.SERVLET); // tomcat 서버 구동
+        application.run(args);
     }
-
 }
