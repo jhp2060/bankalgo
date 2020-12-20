caef2ea8efc308463f9d3a6bd205d812da6d15b7
jhp2060 <jhp2060@gmail.com>
2020-12-20 16:15:50 +0900

    feat(controller): product controller 생성자 메시지 출력

--- src/main/java/com/eveningminusdot/bankalgo/controller/ProductController.java
+++ src/main/java/com/eveningminusdot/bankalgo/controller/ProductController.java
@@ -4,9 +4,12 @@ import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@NoArgsConstructor
 @RestController
 public class ProductController {
+
+    ProductController () {
+        System.out.println("\n\nHELLO THIS IS PRODUCT CONTROLLER\n\n");
+    }
 
     @GetMapping("/hello")
     public String hello(String name) {
