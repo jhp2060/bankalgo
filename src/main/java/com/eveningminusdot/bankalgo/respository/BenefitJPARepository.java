2b861e0083bf6923bf4ed10fc7612843fdc7cab5
jhp2060 <jhp2060@gmail.com>
2020-12-21 23:39:42 +0900

    feat(repository): benefitJPARepository - findByProductId추가

--- src/main/java/com/eveningminusdot/bankalgo/respository/BenefitJPARepository.java
+++ src/main/java/com/eveningminusdot/bankalgo/respository/BenefitJPARepository.java
@@ -1,9 +1,14 @@
 package com.eveningminusdot.bankalgo.respository;
 
 import com.eveningminusdot.bankalgo.domain.Benefit;
+import com.eveningminusdot.bankalgo.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BenefitJPARepository extends JpaRepository<Benefit, Long> {
+    List<Benefit> findByProductId(Long id);
+    List<Benefit> findByProduct(Product product);
 }
