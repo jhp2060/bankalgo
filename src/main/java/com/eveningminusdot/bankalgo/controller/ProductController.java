700666dbed39a05c0fbe80667ffb79fc1b85b70a
jhp2060 <jhp2060@gmail.com>
2020-12-21 23:07:19 +0900

    feat(controller): productController findAll

--- src/main/java/com/eveningminusdot/bankalgo/controller/ProductController.java
+++ src/main/java/com/eveningminusdot/bankalgo/controller/ProductController.java
@@ -1,19 +1,27 @@
 package com.eveningminusdot.bankalgo.controller;
 
-import lombok.NoArgsConstructor;
+import com.eveningminusdot.bankalgo.service.ProductService;
+import com.eveningminusdot.bankalgo.dto.ProductSimpleDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class ProductController {
 
-    ProductController () {
-        System.out.println("\n\nHELLO THIS IS PRODUCT CONTROLLER\n\n");
-    }
+    private final ProductService productService;
 
     @GetMapping("/hello")
     public String hello(String name) {
         return "Hello : " + name;
+    }
+
+    @GetMapping("/api/v1/products")
+    public List<ProductSimpleDto> findAll() {
+        return productService.findAll();
     }
 
 }
