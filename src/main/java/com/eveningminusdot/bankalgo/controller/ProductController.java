fec649ccb6a0fb68bac88da286d1f2135a5b25aa
jhp2060 <jhp2060@gmail.com>
2020-12-22 23:17:21 +0900

    feat(controller): ProductController - findById : product detail

--- src/main/java/com/eveningminusdot/bankalgo/controller/ProductController.java
+++ src/main/java/com/eveningminusdot/bankalgo/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.eveningminusdot.bankalgo.controller;
 
+import com.eveningminusdot.bankalgo.dto.ProductDetailDto;
 import com.eveningminusdot.bankalgo.service.ProductService;
 import com.eveningminusdot.bankalgo.dto.ProductSimpleDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,6 +24,11 @@ public class ProductController {
     @GetMapping("/api/v1/products")
     public List<ProductSimpleDto> findAll() {
         return productService.findAll();
+    }
+
+    @GetMapping("/api/v1/products/{id}")
+    public ProductDetailDto findById(@PathVariable Long id) {
+        return productService.findById(id);
     }
 
 }
