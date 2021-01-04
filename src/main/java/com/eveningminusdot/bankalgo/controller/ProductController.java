d13b059ab2dd1a3e6fab1a0e2233e854205d74c1
jhp2060 <jhp2060@gmail.com>
2021-01-05 00:01:37 +0900

    feat(controller): product controller - findAll에 type Request parameter 추가. 필터기능.

--- src/main/java/com/eveningminusdot/bankalgo/controller/ProductController.java
+++ src/main/java/com/eveningminusdot/bankalgo/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.eveningminusdot.bankalgo.dto.ProductSimpleDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,8 +23,9 @@ public class ProductController {
     }
 
     @GetMapping("/api/v1/products")
-    public List<ProductSimpleDto> findAll() {
-        return productService.findAll();
+    public List<ProductSimpleDto> findAll(
+            @RequestParam(defaultValue = "") String type) {
+        return productService.findAll(type);
     }
 
     @GetMapping("/api/v1/products/{id}")
