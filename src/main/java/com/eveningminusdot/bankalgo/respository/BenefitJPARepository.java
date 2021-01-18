fff43c056ef91ccf04cfd8a7cf5e7fd650d68482
jhp2060 <jhp2060@gmail.com>
2021-01-18 23:49:36 +0900

    feat(repository): benefit repository - product id와 requirement 동시 필터 기능

--- src/main/java/com/eveningminusdot/bankalgo/respository/BenefitJPARepository.java
+++ src/main/java/com/eveningminusdot/bankalgo/respository/BenefitJPARepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface BenefitJPARepository extends JpaRepository<Benefit, Long> {
     List<Benefit> findByProductId(Long id);
     List<Benefit> findByRequirementsContains(Requirement r);
+    List<Benefit> findByProductIdAndRequirementsContains(Long product_id, Requirement r);
 }
 
