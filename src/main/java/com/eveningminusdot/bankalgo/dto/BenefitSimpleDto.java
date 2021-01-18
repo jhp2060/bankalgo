a8fe21b9a21b1db639d7652d5b217cd0880f3382
jhp2060 <jhp2060@gmail.com>
2021-01-18 23:51:49 +0900

    feat(dto): benefit simple dto에 productName 추가

--- src/main/java/com/eveningminusdot/bankalgo/dto/BenefitSimpleDto.java
+++ src/main/java/com/eveningminusdot/bankalgo/dto/BenefitSimpleDto.java
@@ -8,12 +8,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BenefitSimpleDto {
     private Long id;
+    private String productName;
     private String name;
     private String type;
     private String description;
 
     public BenefitSimpleDto(Benefit benefit) {
         this.id = benefit.getId();
+        this.productName = benefit.getProduct().getName();
         this.name = benefit.getName();
         this.type = benefit.getType().toString();
         this.description = benefit.getDescription();
